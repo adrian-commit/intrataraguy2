fa7548d8c953de4be6c4a46c8956162e50695901
adrian-commit <santillanadrianalberto@hotmail.com>
2022-06-08 16:05:16 -0300

    se incorporo una agenda de tareas/calendario

---
+++ public/js/script.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+      },
+      initialDate: new Date(),
+      navLinks: true, // can click day/week names to navigate views
+      businessHours: true, // display business hours
+      editable: true,
+      selectable: true,
+      locale: 'es',
+      buttonText:{
+          today:'Hoy',
+          month:'Mes',
+          week:'Semana',
+          day:'Dia',
+          list:'Lista',
+              },
+
+      events: [
+        {
+          title: 'Cumple Jordana',
+          start: '2022-06-08T13:00:00',
+            end: '2022-06-09T11:00:00',
+          color: 'blue',
+        },
+        {
+          title: 'Vencimiento IVA-DEMONTE',
+          start: '2022-06-08T11:00:00',
+            end: '2022-06-09T11:00:00',
+          color: 'blue',
+        },
+        
+      ]
+    });
+
+    calendar.render();
+  });
